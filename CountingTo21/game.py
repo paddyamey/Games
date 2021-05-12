7c9d1c5fd91aec03d7fa14f3b612ff7dd1b6dcac
Paddy <78871213+paddyamey@users.noreply.github.com>
2021-05-12 17:04:38 +0100

    Create game.py

---
+++ CountingTo21/game.py
@@ -0,0 +1,192 @@
+import random
+
+
+def check_factor(number, factor):
+    """
+    :param number: integer
+    :param factor: integer
+    :return: boolean
+    """
+    if number % factor == 0:
+        return True
+    else:
+        return False
+
+
+def closest_multiple(number, number_m):
+    """
+    :param number: integer
+    :param number_m: integer
+    :return: integer
+    """
+
+    while number % number_m != 0:
+        number += 1
+
+    return number
+
+
+def validate_user_input(numbers_entered, user_input):
+
+    user_list = user_input.split(",")
+
+    #  check no more than three numbers have been entered
+    if len(user_list) > 3:
+        return False
+
+    #  the input should all be separated by comas if they are not the input will be split incorrectly
+    number_checker = [item.isnumeric() for item in user_list]
+    if False in number_checker:
+        return False
+
+    #  check for 1-3 consecutive numbers
+    if len(user_list) == 3:
+        if int(user_list[2]) - int(user_list[0]) != 2:
+            return False
+    elif len(user_list) == 2:
+        if int(user_list[1]) - int(user_list[0]) != 1:
+            return False
+
+    # the input must start after the previous output
+    if int(user_list[0]) - int(numbers_entered[-1]) != 1:
+        return False
+
+
+
+
+
+# coin_toss = random.randint(1, 2)
+coin_toss = 2
+number_list = list(range(1,22))
+
+#TODO: create function to validate user input
+#TODO: create list to keep note of every number used so far
+
+if  coin_toss == 1:
+
+    # the first choice can only be 1, 2 or 3
+    number = random.randint(1, 3)
+
+    # keep the game going until a player says 21
+    while True:
+
+        if number < 4:
+            difference = number
+
+        else:
+            difference = number - user_number
+
+        if difference == 1:
+            print(f"Computer: {number}")
+
+        elif difference == 2:
+            print(f"Computer: {number-1},{number}")
+
+        else:
+            print(f"Computer: {number-2},{number-1},{number}")
+
+
+        # verify player has not entered a number with a difference greater than 3
+        while True:
+
+            user_number_list = input("Input your number separated by comas:").split(",")
+            try:
+                user_number = int(user_number_list[-1])
+
+                if int(user_number) - number < 4:
+                    number = user_number
+                    break
+                elif user_number < number:
+                    print(f"You must enter a number higher than {number}.")
+                else:
+                    print("Invalid input, you can only go up 3 numbers.")
+
+            except ValueError:
+                print("You must enter a number.")
+
+        #TODO: correct logic to prevent user from surpassing 21 if they enter it as e.g. 21,22,23
+        for value in user_number_list:
+            if value == 21:
+                print("You have been defeated by the computer.")
+                break
+
+        if number == 21:
+            print("You have been defeated by the computer.")
+            break
+
+        elif number == 20:
+            print("Computer: 21")
+            print("Congratulations, you have won.")
+            break
+
+        elif number < 4:
+            number = 4
+
+        elif check_factor(number, 4):
+            number += random.randint(1, 3)
+
+        else:
+            number = closest_multiple(number, 4)
+
+else:
+    while True:
+
+        # verify player has not entered a number with a difference greater than 3
+        while True:
+
+            user_number_list = input("Input your number separated by comas:").split(",")
+            try:
+                user_number = int(user_number_list[-1])
+
+                if int(user_number) < 4:
+                    number = user_number
+                    break
+                elif user_number < number:
+                    print(f"You must enter a number higher than {number}.")
+                elif int(user_number) - number < 4:
+                    number = user_number
+                    break
+                else:
+                    print("Invalid input, you can only go up 3 numbers.")
+
+            except ValueError:
+                print("You must enter a number.")
+            except NameError:
+                print("You can only go up 3 numbers.")
+
+
+        if number == 21:
+            print("You have been defeated by the computer.")
+            break
+
+        elif number == 20:
+            print("Computer: 21")
+            print("Congratulations, you have won.")
+            break
+
+        elif number < 4:
+            number = 4
+
+        elif check_factor(number, 4):
+            number += random.randint(1, 3)
+
+        else:
+            number = closest_multiple(number, 4)
+
+        if number < 4:
+            difference = number
+
+        else:
+            difference = number - user_number
+
+        if difference == 1:
+            print(f"Computer: {number}")
+
+        elif difference == 2:
+            print(f"Computer: {number-1},{number}")
+
+        else:
+            print(f"Computer: {number-2},{number-1},{number}")
+
+
+
